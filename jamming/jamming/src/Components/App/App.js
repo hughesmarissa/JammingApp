490e42f5bbd5735952b4a3f098e63d3288a9ab14
Marissa Hughes <hughes.marissa331@gmail.com>
2021-10-09 11:34:01 -0700

    Add app CSS &  Search Bar Directory

---
+++ jamming/jamming/src/Components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+          {/* <SearchBar /> */}
+            <div className="App-playlist">
+            {/* <SearchResults /> */}
+            {/* <Playlist /> */}
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
