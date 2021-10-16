19404a08a59ba4d647f25f001f30b7ab440572e4
Marissa Hughes <hughes.marissa331@gmail.com>
2021-10-16 12:24:53 -0700

    Add SearchBar/ and CSS in Components/

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
