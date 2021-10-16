8fe360794e1833e22c286db8bf044999aaa1e12c
Marissa Hughes <hughes.marissa331@gmail.com>
2021-10-16 12:41:32 -0700

    Add Playlist/ and CSS in Components/

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Tracklist.css';
+
+class Tracklist extends React.Component {
+  render() {
+    return (
+      <div class="TrackList">
+          {/* <!-- You will add a map method that renders a set of Track components  --> */}
+      </div>
+    );
+  }
+}
+
+export default Tracklist;
